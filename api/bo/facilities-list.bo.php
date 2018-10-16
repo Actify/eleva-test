0fd9e2dda7c6b51278dc5c9b466718e6673fa76c
matteo <m.zerbi@shaa.it>
2018-10-16 17:50:24 +0200

    final commit

--- api/bo/facilities-list.bo.php
+++ api/bo/facilities-list.bo.php
@@ -34,6 +34,40 @@
       return $rows;
     }
 
+    /*public function listNearby($userId, $lat, $lng, $radius, $page) {
+
+      $sql= 'SELECT a.IDEVENT, a.TITLE, a.DTCREAT, a.IDOWNER, a.DTSTART, a.DTEND, a.DTENDSUBSCRIB, a.ORGPLACE, a.EVENTPLACE, a.EVENTPLACELAT, a.EVENTPLACELNG, a.MINPART, a.MAXPART, a.NOTES, a.COVERIMG, a.VISIBILITY, a.WHOLEDAY, a.STATUS, b.NAME AS OWNER_NAME, b.SURNAME AS OWNER_SURNAME, b.USERTYPE AS OWNER_USERTYPE, b.URLPROFILEPIC as OWNER_URLPROFILEPIC, IFNULL(e.STATUS,'.SingleEventBO::STATUS_UNDEF.') AS PART_STATUS, e.IDSENDER, ';
+      $sql.= '0 AS DIFF, EVENTPLACEDIST ';
+      $sql.= 'FROM (';
+      $sql.= 'SELECT '.str_replace('a.','aa.','a.IDEVENT, a.TITLE, a.DTCREAT, a.IDOWNER, a.DTSTART, a.DTEND, a.DTENDSUBSCRIB, a.ORGPLACE, a.EVENTPLACE, a.EVENTPLACELAT, a.EVENTPLACELNG, a.MINPART, a.MAXPART, a.NOTES, a.COVERIMG, a.VISIBILITY, a.WHOLEDAY, a.STATUS').', p.radius, p.distance_unit ';
+          $sql.= '* DEGREES(ACOS(COS(RADIANS(p.latpoint)) ';
+          $sql.= '* COS(RADIANS(aa.EVENTPLACELAT)) ';
+          $sql.= '* COS(RADIANS(p.longpoint - aa.EVENTPLACELNG)) ';
+          $sql.= '+ SIN(RADIANS(p.latpoint)) ';
+          $sql.= '* SIN(RADIANS(aa.EVENTPLACELAT)))) AS EVENTPLACEDIST ';
+      $sql.= 'FROM events AS aa ';
+      $sql.= 'JOIN (';
+          $sql.= "SELECT $lat  AS latpoint,  $lng AS longpoint, $radius AS radius, 111.045 AS distance_unit) AS p ";
+      $sql.= 'WHERE aa.EVENTPLACELAT ';
+      $sql.= 'BETWEEN p.latpoint  - (p.radius / p.distance_unit) ';
+          $sql.= 'AND p.latpoint  + (p.radius / p.distance_unit) ';
+      $sql.= 'AND aa.EVENTPLACELNG ';
+      $sql.= 'BETWEEN p.longpoint - (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint)))) ';
+          $sql.= 'AND p.longpoint + (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint)))) ';
+      $sql.= ') AS a ';
+      $sql.= 'WHERE a.EVENTPLACEDIST <= a.radius ';
+      $sql.= 'ORDER BY date(DTSTART), EVENTPLACEDIST';
+      $categoriesBO=new CategoriesBO();
+      $rows=array();
+      $result=$mysqli->query($sql);
+      while($row = $result->fetch_object()) {
+        $row->DIFF=time()-strtotime($row->DTCREAT);
+        $row->CATEGORIES=$categoriesBO->getEventCategories($row->IDEVENT);
+        $rows[] = $row;
+      }
+      return getResponse($rows);
+    }*/
+
     public function search($str = '') {
       $facilityBO = new FacilityBO($this->conn);
       
